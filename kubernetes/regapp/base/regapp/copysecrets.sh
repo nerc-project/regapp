a6fe4ded79bdef12a29d798513572ed291416d99
Jim Culbert <jim@culbert.net>
2022-09-12 15:36:06 -0400

    added idps and whitelist email domains (#23)
    * added idps and whitelist email domains
    
    * update email domains in kc configmap as well as cilogon one
    
    * add whitelist email domains to the kc configmap

--- kubernetes/regapp/base/regapp/copysecrets.sh
+++ kubernetes/regapp/base/regapp/copysecrets.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-kubectl get secret keycloak-client-secret-regapp -n keycloak -o json | \
+kubectl get secret keycloak-client-secret-regapp-client -n keycloak -o json | \
 jq .metadata='{"namespace": "regapp", "name": "keycloak-client-secret-regapp"}' | \
 kubectl apply -f -
